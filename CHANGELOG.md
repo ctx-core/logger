b9f71faa2568154504dc4b5a36cfb5f18938dd2b
Brian Takita <brian.takita@gmail.com>
2020-09-19 15:17:00 -0700

    typescript strict checking

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,13 @@
 # @ctx-core/logger
 
+## 4.0.0
+
+### Major Changes
+
+- Typescript strict checking
+
 ## 3.1.29
+
 ### Patch Changes
 
 - fix: npm run compile: path to tsc
--- lib.d.ts
+++ lib.d.ts
@@ -1,8 +1,8 @@
-export declare function debug(...arg_a1: any[]): any;
-export declare function log(...arg_a1: any[]): any;
-export declare function info(...arg_a1: any[]): any;
-export declare function warn(...arg_a1: any[]): any;
-export declare function error(...arg_a1: any[]): any;
+export declare function debug(...arg_a1: any[]): void;
+export declare function log(...arg_a1: any[]): void;
+export declare function info(...arg_a1: any[]): void;
+export declare function warn(...arg_a1: any[]): void;
+export declare function error(...arg_a1: any[]): void;
 export declare const error__log: typeof error;
 export declare function _log(message: any, fn: any): (...arg_a1: any[]) => any;
 export declare function _console(fn: any, ctx__log: any): (...arg_a1: any[]) => any;
