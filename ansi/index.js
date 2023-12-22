c015b0d84621d59d1d0d53325a5db16901b4364b
Brian Takita <brian.takita@gmail.com>
2023-12-21 21:56:34 -0500

    fix: eslint errors

--- ansi/index.js
+++ ansi/index.js
@@ -22,33 +22,28 @@ export {
 	ansi_error as chalk_error,
 }
 function debug_ansi_a_(...arg_a) {
-	return use_ansi ? [
-			`\x1B[37m${arg_a.join(' ')}\x1B[39m`
-		] // chalk.white
-									: arg_a
+	return use_ansi
+		? [`\x1B[37m${arg_a.join(' ')}\x1B[39m`] /* chalk.white */
+		: arg_a
 }
 function log_ansi_a_(...arg_a) {
-	return use_ansi ? [
-			`\x1B[90m${arg_a.join(' ')}\x1B[39m`
-		] // chalk.grey
-									: arg_a
+	return use_ansi
+		? [`\x1B[90m${arg_a.join(' ')}\x1B[39m`] /* chalk.grey */
+		: arg_a
 }
 function info_ansi_a_(...arg_a) {
-	return use_ansi ? [
-			`\x1B[32m${arg_a.join(' ')}\x1B[39m`
-		] // chalk.green
-									: arg_a
+	return use_ansi
+		? [`\x1B[32m${arg_a.join(' ')}\x1B[39m`] // chalk.green
+		: arg_a
 }
 function warn_ansi_a_(...arg_a) {
-	return use_ansi ? [
-			`\x1B[31m${arg_a.join(' ')}\x1B[39m`
-		] // chalk.red
-									: arg_a
+	return use_ansi
+		? [`\x1B[31m${arg_a.join(' ')}\x1B[39m`] /* chalk.red */
+		: arg_a
 }
 function error_ansi_a_(...arg_a) {
-	return use_ansi ? [
-			`\x1B[31m\x1B[1m${arg_a.join(' ')}\x1B[22m\x1B[39m`
-		] // chalk.red.bold
-									: arg_a
+	return use_ansi
+		? [`\x1B[31m\x1B[1m${arg_a.join(' ')}\x1B[22m\x1B[39m`] /* chalk.red.bold */
+		: arg_a
 }
 
